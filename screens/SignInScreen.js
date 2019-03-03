85d0658e980a3e61f7661b6e0b0dc487f7cbc790
Skyler <bitsandbytes001@gmail.com>
2019-03-02 23:13:06 -0500

    ADDS OAUTH PROPERLY WOOOOO

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -3,32 +3,95 @@ import {
     ScrollView,
     Text,
     View,
-
+    AsyncStorage
 } from 'react-native';
 import { AuthSession } from 'expo';
-
+import firebase from 'firebase';
 import * as Styles from "../constants/Styles";
 import {StyledButton} from "../components/StyledButton";
+import getGithubTokenAsync from "../utils/GithubAuth";
+import * as Secrets from "../conf";
 
 const googleButtonText = "Sign In With Google";
 const whateverButtonTExt = "Sign In With x";
 const styles = Styles.globalStyles();
 
-const APP_ID = 'the app id';
+const GithubStorageKey = '@Expo:GithubToken';
 
+const firebaseConfig = {
+    apiKey: Secrets.FIREBASE_API_KEY,
+    projectId: Secrets.FIREBASE_PROJECT_ID,
+};
+
+function initializeFirebase() {
+    // Prevent reinitializing the app in snack.
+    if (!firebase.apps.length) {
+        return firebase.initializeApp(firebaseConfig);
+    }
+}
+
+
+async function signInAsync(token) {
+    try {
+        if (!token) {
+            const token = await getGithubTokenAsync();
+            if (token) {
+                await AsyncStorage.setItem(GithubStorageKey, token);
+                return signInAsync(token);
+            } else {
+                return;
+            }
+        }
+        const credential = firebase.auth.GithubAuthProvider.credential(token);
+        return firebase.auth().signInAndRetrieveDataWithCredential(credential);
+    } catch ({ message }) {
+        alert(message);
+    }
+}
+
+async function signOutAsync() {
+    try {
+        await AsyncStorage.removeItem(GithubStorageKey);
+        await firebase.auth().signOut();
+    } catch ({ message }) {
+        alert('Error: ' + message);
+    }
+}
+
+async function attemptToRestoreAuthAsync() {
+    let token = await AsyncStorage.getItem(GithubStorageKey);
+    if (token) {
+        console.log('Sign in with token', token);
+        return signInAsync(token);
+    }
+}
 
 export default class SignInScreen extends React.Component {
+    state = { isSignedIn: false };
 
-    state = {
-        result: null,
+    componentDidMount() {
+        this.setupFirebaseAsync();
     }
+
+    setupFirebaseAsync = async () => {
+        initializeFirebase();
+
+        firebase.auth().onAuthStateChanged(async auth => {
+            const isSignedIn = !!auth;
+            this.setState({ isSignedIn });
+            if (!isSignedIn) {
+                attemptToRestoreAuthAsync();
+            }
+        });
+    };
+
 
     render() {
         return (
             <View style={styles.container}>
                 <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
                     <View style={styles.SplashContainer}>
-                        <StyledButton onPress={this._handlePressAsync} title={googleButtonText}/>
+                        <StyledButton onPress={() => signInAsync()} title={googleButtonText}/>
                         {this.state.result ? (
                             <Text>{JSON.stringify(this.state.result)}</Text>
                         ): null }
@@ -44,9 +107,11 @@ export default class SignInScreen extends React.Component {
         let redirectUrl = AuthSession.getRedirectUrl();
         let result = await AuthSession.startAsync({
             authUrl:
-                `https://www.facebook.com/v2.8/dialog/oauth?response_type=token` +
-                `&client_id= the client id owo` +
-                `&redirect_uri=${encodeURIComponent(redirectUrl)}`,
+                `https://github.com/login/oauth/authorize` +
+                `?client_id=` + github.CLIENT_ID +
+                `&client_secret=` + github.CLIENT_SECRET +
+                `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+                `&scope=${encodeURIComponent(githubFields.join(' '))}`,
         });
         this.setState({ result });
     };
