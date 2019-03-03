9b638fc876a07979ddde70286b373cd5f6b7cb86
Skyler <bitsandbytes001@gmail.com>
2019-03-03 11:33:52 -0500

    adds pin dropping (but not permanently)

--- screens/AddMemory.js
+++ screens/AddMemory.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {
+    TextInput,
     View,
+    Text
 } from 'react-native';
 import * as Styles from "../constants/Styles";
+import * as LocationsFile from "../utils/LocationsFile"
 import {StyledButton} from "../components/StyledButton";
 import Camera from "./Camera";
 const styles = Styles.globalStyles();
@@ -15,22 +18,57 @@ render() {
     const { navigate } = this.props.navigation;
     const type = this.props.navigation.getParam('type', 'picture');
     const location = this.props.navigation.getParam('location', null);
-    console.log(type);
-    console.log(location);
 
     return (
         <View style={styles.container}>
             <View style={styles.container}>
                 <Camera/>
             </View>
+            <View style={{padding: 10}}>
+                <TextInput
+                    style={{height: "10%"}}
+                    placeholder="Title"
+                    onChangeText={(title) => this.setState({title})}
+                />
+                <TextInput
+                    style={{height: "50%"}}
+                    placeholder="Description"
+                    onChangeText={(description) => this.setState({description})}
+                />
+            </View>
             <View style={styles.backButton}>
-                <StyledButton title={"save"} onPress={()=>AddMemory._save(navigate)}/>
+                <StyledButton title={"save"} onPress={()=>AddMemory._save(navigate, location, this.state)}/>
             </View>
         </View>
         );}
 
 
-    static _save(navigate) {
+    static _save(navigate, location, text) {
+    console.log(text);
+        let description;
+        let title;
+    if(text){
+        if(text.description){
+            description = text.description;
+        }else{
+            description = "none given"
+        }
+        if(text.title){
+            title = text.title;
+        } else{
+            title = "good place"
+        }
+    }
+        LocationsFile.markers.push(
+            {
+                coordinate: {
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                },
+                title: title,
+                description: description,
+            },
+        )
         navigate("Main")
     }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,17 +7,15 @@ import {Location, MapView, Permissions} from 'expo';
 import * as Styles from "../constants/Styles";
 import {LoadingIcon} from "../components/LoadingIcon";
 import CircleButton from 'react-native-circle-button';
+import * as LocationsFile from "../utils/LocationsFile";
 
 const styles = Styles.globalStyles();
+let markers = LocationsFile.markers;
+let location;
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
-  };
-  state = {
-    mapRegion: null,
-    hasLocationPermissions: false,
-    locationResult: null
   };
 
   componentDidMount() {
@@ -39,15 +37,21 @@ export default class HomeScreen extends React.Component {
       this.setState({ hasLocationPermissions: true });
     }
 
-    let location = await Location.getCurrentPositionAsync({});
+    location = await Location.getCurrentPositionAsync({});
     this.setState({ locationResult: JSON.stringify(location) });
 
   this.setState({mapRegion: { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }});
 };
 
+  state = {
+    markers : markers,
+    mapRegion: null,
+    hasLocationPermissions: false,
+    locationResult: null
+  };
+
   render() {
     return (
-
           <View style={styles.container}>
             {
               this.state.locationResult === null ?
@@ -63,11 +67,25 @@ export default class HomeScreen extends React.Component {
                       this.state.mapRegion === null ?
                           <Text>Map region doesn't exist.</Text> :
                           <View style={styles.container}>
+
                             <MapView
                               style={styles.map}
+                              customMapStyle={Styles.mapStyle}
                               region={this.state.mapRegion}
                               onRegionChange={this._handleMapRegionChange}
-                          />
+                              showsUserLocation={true}
+                              showsMyLocationButton={true}
+                          >
+                              {this.state.markers.map((marker, index) => {
+                                return (
+                                    <MapView.Marker
+                                        key={index}
+                                        coordinate={marker.coordinate}
+                                        title={marker.title}
+                                        description={marker.description}/>
+                                );
+                              })}
+                            </MapView>
                             <View style={styles.memoryAddOverlay}>
                               <CircleButton
                                   primaryColor={'#ffd44d'}
@@ -77,10 +95,10 @@ export default class HomeScreen extends React.Component {
                                   iconButtonLeft={require("../assets/images/secret.png")}
                                   iconButtonRight={require("../assets/images/friend.png")}
 
-                                  onPressButtonTop={() => this.props.navigation.navigate('AddMemory', { type: "picture", location:this.state.locationResult})}
-                                  onPressButtonDown={() => this.props.navigation.navigate('AddMemory', { type: "write", location:this.state.locationResult})}
-                                  onPressButtonLeft={() => this.props.navigation.navigate('AddMemory', { type: "picture", location:this.state.locationResult})}
-                                  onPressButtonRight={() => this.props.navigation.navigate('AddMemory', { type: "picture", location:this.state.locationResult})}
+                                  onPressButtonTop={() => this.props.navigation.navigate('AddMemory', { type: "picture", location})}
+                                  onPressButtonDown={() => this.props.navigation.navigate('AddMemory', { type: "write", location})}
+                                  onPressButtonLeft={() => this.props.navigation.navigate('AddMemory', { type: "picture", location})}
+                                  onPressButtonRight={() => this.props.navigation.navigate('AddMemory', { type: "picture", location})}
                                   />
                             </View>
                           </View>
